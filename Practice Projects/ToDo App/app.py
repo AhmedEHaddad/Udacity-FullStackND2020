8fa853ed140920607185c334a3c2d03768bc1d8b
Ahmed <ahmed.e.h.2.55@gmail.com>
2020-04-05 05:30:46 +0300

    Todo starter

---
+++ Practice Projects/ToDo App/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://devuser:devpass@localhost:5432/todoDB'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    #return 'Welcome to TODO App!' 
+    render_template('index.html', data=[{
+        'description': 'Todo 1'
+    },{
+        'description': 'Todo 2'
+    },{
+        'description': 'Todo 3'
+    
+    }])
