e4dcb1b055727d5811fdd679d6cbc28917c6b458
Ahmed <ahmed.e.h.2.55@gmail.com>
2020-04-07 15:22:52 +0300

    #TODO app user input

--- Practice Projects/ToDo App/app.py
+++ Practice Projects/ToDo App/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -8,17 +8,26 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://devuser:devpass@localhost:
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
 
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
 
 
 @app.route('/')
 def index():
     #return 'Welcome to TODO App!' 
-    return render_template('index.html', data=[{
-        'description': 'Todo 1'
-    },{
-        'description': 'Todo 2'
-    },{
-        'description': 'Todo 3'
-    
-    }])
+    return render_template('index.html', data=Todo.query.all())
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+  description = request.form.get('description', '')
+  todo = Todo(description=description)
+  db.session.add(todo)
+  db.session.commit()
+  return redirect(url_for('index'))
