c5a6d6f5889d775d0d1883fffef236dde605eb42
Ahmed <ahmed.e.h.2.55@gmail.com>
2020-04-09 07:25:33 +0300

    #TODO: #Relationship modeling

--- Practice Projects/ToDo App/app.py
+++ Practice Projects/ToDo App/app.py
@@ -11,18 +11,32 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
+#############################################################
+##Models
+#############################################################
 class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
   completed = db.Column(db.Boolean, nullable=False, default=False)
+  list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
 
 #db.create_all() #made obselete by migrations versions
+class TodoList(db.Model):
+  __tablename__ = 'todolists'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+  #completed = db.Column(db.Boolean, nullable=False, default=False)
+  todoss = db.relationship('Todo', backref ='list', lazy=True)
+  
 
 
+#############################################################
+##Controllers
+############################################################
 @app.route('/')
 def index():
     #return 'Welcome to TODO App!' 
